c905cd0be48024042b6e4b911bdd17403f09239e
Anthony Powles <github@idreamz.net>
2016-04-06 22:05:16 -0700

    Add basic gin server

---
+++ manager.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+)
+
+func init() {
+	log.Println("Deeva Manager!")
+}
+
+func main() {
+	router := gin.Default()
+	router.POST("/run", handleRunRequest)
+
+	port := os.Getenv("DEEVA_MANAGER_PORT")
+	if len(port) == 0 {
+		port = "9090"
+	}
+
+	log.Printf("Starting in %s mode on port %s\n", gin.Mode(), port)
+	host := fmt.Sprintf(":%s", port)
+	router.Run(host)
+}
+
+func handleRunRequest(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{})
+}
